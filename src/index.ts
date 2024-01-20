9f15160dd7bad7c83982806a88176043794b9d4b
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-01-20 20:06:57 +0900

    Refactor functions to individual exports in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @param {string | Uint8Array} data
  * @returns {string}
  */
-const encodeZW = (data: string | Uint8Array): string => {
+export const encodeZW = (data: string | Uint8Array): string => {
   if (typeof data === "string") {
     return encodeZW(new TextEncoder().encode(data));
   }
@@ -17,51 +17,36 @@ const encodeZW = (data: string | Uint8Array): string => {
  * @param {string} text string containing ZWTs (zero-width-text)
  * @returns {Uint8Array[]}
  */
-const decodeZWsToUint8Array = (text: string) =>
+export const decodeZWsToUint8Array = (text: string) =>
   Array.from(text.matchAll(/\u200c([\u200b\u200d]+)\u200c/g))
     .map((re) => re[1].replace(/\u200b/g, "0").replace(/\u200d/g, "1"))
-    .map(
-      (zo) =>
-        new Uint8Array((zo.match(/.{8}/g) || []).map((b) => parseInt(b, 2))),
-    );
+    .map((zo) => new Uint8Array((zo.match(/.{8}/g) || []).map((b) => parseInt(b, 2))));
 
 /**
  * Decode ZWTs (zero-width-texts) in text into string[]
  * @param {string} text string containing ZWTs (zero-width-text)
  * @returns {string[]}
  */
-const decodeZWs = (text: string) =>
-  decodeZWsToUint8Array(text).map((a) => new TextDecoder().decode(a));
+export const decodeZWs = (text: string) => decodeZWsToUint8Array(text).map((a) => new TextDecoder().decode(a));
 
 /**
  * Decode ZWT (zero-width-text) in text into string[]
  * @param {string} text string containing ZWTs (zero-width-texts)
- * @param {number} i index of ZWTs (zero-width-texts)
+ * @param {number} index index of ZWTs (zero-width-texts)
  * @returns {string}
  */
-const decodeZW = (text: string, i = 0) => decodeZWs(text)[i] ?? "";
+export const decodeZW = (text: string, index = 0) => decodeZWs(text)[index] ?? "";
 
 /**
  * Strip ZWTs (zero-width-texts) from text
  * @param {string} text
  * @returns {string}
  */
-const stripZW = (text: string) =>
-  text.replace(/\u200c(?:[\u200b\u200d]{8})+\u200c/g, "");
+export const stripZW = (text: string) => text.replace(/\u200c(?:[\u200b\u200d]{8})+\u200c/g, "");
 
 /**
  * Reveal ZWTs (zero-width-texts) in text
  * @param {string} text string containing ZWTs (zero-width-texts)
  * @returns {string}
  */
-const revealZW = (text: string) =>
-  text.replace(/\u200c(?:[\u200b\u200d]{8})+\u200c/g, (z) => decodeZW(z));
-
-export default {
-  encodeZW,
-  decodeZW,
-  decodeZWs,
-  decodeZWsToUint8Array,
-  revealZW,
-  stripZW,
-};
+export const revealZW = (text: string) => text.replace(/\u200c(?:[\u200b\u200d]{8})+\u200c/g, (z) => decodeZW(z));
